fd8889afef1e80a3405852865e86396fd35964d9
Stas <smaster950@gmail.com>
2022-10-03 22:50:04 +0300

    Complete Exercise8

--- Exercise8/Program.cs
+++ Exercise8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string number1 = Console.ReadLine() ?? "";
+int number1A = int.Parse(number1);
+
+int count = 2;
+
+while(count <= number1A)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
