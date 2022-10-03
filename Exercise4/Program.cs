7eea667079cecccff07668e94f92b509ebda0f24
Stas <smaster950@gmail.com>
2022-10-03 20:25:56 +0300

    Complete Exercise4

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine() ?? "";
+int number1A = int.Parse(number1);
+Console.Write("Введите второе число: ");
+string number2 = Console.ReadLine() ?? "";
+int number2A = int.Parse(number2);
+Console.Write("Введите третье число: ");
+string number3 = Console.ReadLine() ?? "";
+int number3A = int.Parse(number3);
+
+int max = number1A;
+
+if(number2A > max)
+{
+    max = number2A;
+}
+if (number3A > max)
+{
+    max = number3A;
+}
+else
+{
+    max = number1A;
+}
+Console.WriteLine("max = " + max);
