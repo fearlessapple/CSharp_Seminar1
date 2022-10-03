7c7e76dee1923b099a67aceb16614e57d1cfc906
Stas <smaster950@gmail.com>
2022-10-03 19:41:41 +0300

    Complete Exercise2 project

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine() ?? "";
+int number1A = int.Parse(number1);
+Console.Write("Введите второе число: ");
+string number2 = Console.ReadLine() ?? "";
+int number2A = int.Parse(number2);
+if(number1A > number2A)
+{
+    int max = number1A;
+    Console.WriteLine("a=" + number1A + " b=" + number2A + " -> max = " + max);
+}
+else
+{
+    int max = number2A;
+    Console.WriteLine("a=" + number1A + " b=" + number2A + " -> max = " + max);
+}
