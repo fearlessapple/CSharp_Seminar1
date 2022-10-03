5fc26aefc6bfe14bd717228689a91cd933a92eb2
Stas <smaster950@gmail.com>
2022-10-03 20:43:42 +0300

    Complete Exercise6

--- Exercise6/Program.cs
+++ Exercise6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string number1 = Console.ReadLine() ?? "";
+int number1A = int.Parse(number1);
+
+if(number1A % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
